bdcaca0b2fe0c3f494fd14d6df413e4e721653b2
Dion Häfner <dion.haefner@simulation.science>
2026-05-27 11:34:26 +0200

    delete code ratio and `tesseracts` command

--- mosaic/_version.py
+++ mosaic/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '2.dev37+neurips.geafdc2830.d20260526'
-__version_tuple__ = version_tuple = (2, 'dev37', 'neurips.geafdc2830.d20260526')
+__version__ = version = '0.2.dev1+paper.repro.gfbc42427c.d20260527'
+__version_tuple__ = version_tuple = (0, 2, 'dev1', 'paper.repro.gfbc42427c.d20260527')
 
 __commit_id__ = commit_id = None
